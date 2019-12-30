5e0ba1e72a3322c58311964ac6ef456209020716
Tom Czarniecki <tomcz@users.noreply.github.com>
2019-12-30 02:49:57 -0500

    allow server address & message to be configured

--- cmd/example-client/main.go
+++ cmd/example-client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
@@ -12,23 +13,27 @@ import (
 )
 
 func main() {
+	addr := flag.String("addr", "localhost:8000", "server address")
+	msg := flag.String("msg", "G'day", "message to send")
+	flag.Parse()
+
 	// Fatal logging prevents defer from firing, so wrap the
 	// service configuration & startup in a realMain function.
-	if err := realMain(); err != nil {
+	if err := realMain(*addr, *msg); err != nil {
 		log.Fatalln("application failed with", err)
 	}
 	log.Println("application stopped")
 }
 
-func realMain() error {
-	conn, err := grpc.Dial("localhost:8000", grpc.WithInsecure())
+func realMain(addr, msg string) error {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
 	client := api.NewExampleClient(conn)
-	res, err := client.Echo(context.Background(), &api.EchoRequest{Message: "G'day"})
+	res, err := client.Echo(context.Background(), &api.EchoRequest{Message: msg})
 	if err != nil {
 		return err
 	}
