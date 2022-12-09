4815bd6436ebb17e1d2addfc1158b624c20273bf
Tom Czarniecki <tomcz@users.noreply.github.com>
2022-12-10 10:35:36 +1100

    random hex is good enough

--- server/random.go
+++ server/random.go
@@ -1,29 +1,17 @@
 package server
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"encoding/hex"
 	"strings"
-	"time"
 )
-
-// From https://www.grc.com/ppp.htm
-const letters = "ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789"
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
 
 func ErrorID() string {
 	// short enough to remember
 	// long enough to be unique-ish
-	return RandString(7)
-}
-
-func RandString(length int) string {
-	var sb strings.Builder
-	sb.Grow(length)
-	for i := 0; i < length; i++ {
-		sb.WriteByte(letters[rand.Intn(len(letters))])
+	buf := make([]byte, 4)
+	if _, err := rand.Read(buf); err != nil {
+		return "XXXX" // unlikely
 	}
-	return sb.String()
+	return strings.ToUpper(hex.EncodeToString(buf))
 }
