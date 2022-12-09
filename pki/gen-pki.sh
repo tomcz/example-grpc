05324699235ac31f44ab55968ff2cd797f776fcb
Tom Czarniecki <tomcz@users.noreply.github.com>
2022-12-10 09:32:08 +1100

    more readable command flag order

--- pki/gen-pki.sh
+++ pki/gen-pki.sh
@@ -9,8 +9,8 @@ openssl req -x509 -new -nodes -sha256 \
         -addext basicConstraints=critical,CA:TRUE,pathlen:0 \
         -addext keyUsage=cRLSign,keyCertSign \
         -subj "/CN=ExampleCA/C=AU/ST=NSW/L=Sydney" \
-        -key ca.key \
         -days 1825 \
+        -key ca.key \
         -out ca.crt
 
 ## Generate server TLS key & certificate
@@ -19,9 +19,9 @@ openssl genrsa -out server.key 2048
 
 openssl req -new -key server.key -out server.csr -config server-csr.conf
 
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key \
-        -CAcreateserial -out server.crt -days 1825 \
-        -extensions v3_ext -extfile server-csr.conf
+openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -days 1825 \
+        -extensions v3_ext -extfile server-csr.conf \
+        -in server.csr -out server.crt
 
 # Generate Alice's TLS key & certificate
 
@@ -29,9 +29,9 @@ openssl genrsa -out alice.key 2048
 
 openssl req -new -key alice.key -out alice.csr -config alice-csr.conf
 
-openssl x509 -req -in alice.csr -CA ca.crt -CAkey ca.key \
-        -CAcreateserial -out alice.crt -days 1825 \
-        -extensions v3_ext -extfile alice-csr.conf
+openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -days 1825 \
+        -extensions v3_ext -extfile alice-csr.conf \
+        -in alice.csr -out alice.crt
 
 # Generate Bob's TLS key & certificate
 
@@ -39,10 +39,10 @@ openssl genrsa -out bob.key 2048
 
 openssl req -new -key bob.key -out bob.csr -config bob-csr.conf
 
-openssl x509 -req -in bob.csr -CA ca.crt -CAkey ca.key \
-        -CAcreateserial -out bob.crt -days 1825 \
-        -extensions v3_ext -extfile bob-csr.conf
+openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -days 1825 \
+        -extensions v3_ext -extfile bob-csr.conf \
+        -in bob.csr -out bob.crt
 
 # Cleanup
 
-rm *.csr
+rm ./*.csr
